eb4ba8320d449eb594f1dfd0f8b5855e66e0f272
Arthur Chiao <chiaoarthur@gmail.com>
2016-11-30 04:57:39 -0800

    Adding basic functionality to backend

--- models/tables.py
+++ models/tables.py
@@ -7,20 +7,15 @@
 # There is an implicit 'id integer autoincrement' field
 # Consult manual for more options, validators, etc.
 
-import datetime
-
-db.define_table('post',
-                Field('user_email', default=auth.user.email if auth.user_id else None),
-                Field('post_content', 'text'),
-                Field('created_on', 'datetime', default=datetime.datetime.utcnow()),
-                Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
-                )
-
-# I don't want to display the user email by default in all forms.
-db.post.user_email.readable = db.post.user_email.writable = False
-db.post.post_content.requires = IS_NOT_EMPTY()
-db.post.created_on.readable = db.post.created_on.writable = False
-db.post.updated_on.readable = db.post.updated_on.writable = False
+db.define_table('movies',
+                Field('movie_id', 'integer', required=True),
+                Field('movie_name', 'string', required=True),
+                Field('director', 'string', required=True),
+                Field('rating', 'float', required=False),
+                Field('genre', 'string', required=True),
+                Field('description', 'text', required=False, default=''),
+                Field('trailer_url', 'string'),
+                Field('poster_url', 'upload'))
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
