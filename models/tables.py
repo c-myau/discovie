2da7d7a19d85cc653dbb910dbd1ce888fe3e29cc
Arthur Chiao <chiaoarthur@gmail.com>
2016-12-01 13:10:38 -0800

    Changes to db schema

--- models/tables.py
+++ models/tables.py
@@ -11,11 +11,21 @@ db.define_table('movies',
                 Field('movie_id', 'integer', required=True),
                 Field('movie_name', 'string', required=True),
                 Field('director', 'string', required=True),
+                Field('writers', 'string', required=True),
+                Field('cast_members', 'string', required=True),
+                Field('mpaa_rating', 'string', required=True),
                 Field('rating', 'float', required=False),
+                Field('votes', 'float', required=False),
                 Field('genre', 'string', required=True),
+                Field('running_time', 'string', required=True),
                 Field('description', 'text', required=False, default=''),
                 Field('trailer_url', 'string'),
                 Field('poster_url', 'upload'))
+
+db.define_table('movie_suggestions',
+                Field('movie_id', 'integer', required=True),
+                Field('user_id', 'integer', required=True))
+
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
