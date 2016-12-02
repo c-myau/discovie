7b79b86b7fc72300a6afa66c111c6d1b421091cb
Arthur Chiao <chiaoarthur@gmail.com>
2016-12-01 16:18:19 -0800

    Minor edits

--- models/db.py
+++ models/db.py
@@ -90,10 +90,12 @@ auth.settings.reset_password_requires_verification = True
 ######################
 # Logging
 import logging, sys
+
 FORMAT = "%(asctime)s %(levelname)s %(process)s %(thread)s %(funcName)s():%(lineno)d %(message)s"
 logging.basicConfig(stream=sys.stderr)
 logger = logging.getLogger(request.application)
 logger.setLevel(logging.INFO)
 
 # Let's log the request.
-logger.info("====> Request: %r %r %r %r" % (request.env.request_method, request.env.path_info, request.args, request.vars))
+logger.info(
+    "====> Request: %r %r %r %r" % (request.env.request_method, request.env.path_info, request.args, request.vars))
