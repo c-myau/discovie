3aed047ad1db7f146400d765576f8281c8159cf1
Miao-Long <myau.christopher@gmail.com>
2016-12-04 04:25:08 -0800

    merged code manually, search now working;

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -1,87 +1,46 @@
 // This is the js for the default/index.html view.
 
-var app = function () {
+var app = function() {
 
     var self = {};
 
     Vue.config.silent = false; // show all warnings
 
-    // Extends an array
-    self.extend = function (a, b) {
-        for (var i = 0; i < b.length; i++) {
-            a.push(b[i]);
-        }
-    };
 
     // Enumerates an array.
-    var enumerate = function (v) {
-        var k = 0;
-        return v.map(function (e) {
-            e._idx = k++;
-        });
+    var enumerate = function(v) {
+        var k=0;
+        return v.map(function(e) {e._idx = k++;});
     };
 
-    // Decorate the array of images.
-    var decorate = function (v) {
-        return v.map(function (e) {
-            e._pending = false;
-        });
+    self.get_movies = function () {
+      //alert('Hello ' + this.name + '!');
+      $.get(movies_url, $.param({q: self.vue.movie_search}), function(data) {
+          //alert('Search Terms: ' + self.vue.movie_search);
+          self.vue.movie_list = data.movie_list;
+          enumerate(self.vue.movie_list);
+      });
+
     };
 
-    self.get_info = function () {
-        $.getJSON(get_info_url, function (data) {
-            self.vue.image_list = data.image_list;
-            enumerate(self.vue.image_list);
-            decorate(self.vue.image_list);
-        });
-    };
-
-    self.mouse_over = function (img_idx, star_idx) {
-        self.vue.image_list[img_idx].num_stars_display = star_idx;
-    };
-
-    self.mouse_out = function (img_idx) {
-        self.vue.image_list[img_idx].num_stars_display = self.vue.image_list[img_idx].num_stars;
-    };
-
-    self.set_stars = function (img_idx, star_idx) {
-        var img = self.vue.image_list[img_idx];
-        img.num_stars = star_idx;
-        img._pending = true;
-        self.vue.$set(self.vue.image_list, img_idx, img);
-        $.post(vote_url,
-            {
-                image_id: img.id,
-                num_stars: star_idx
-            },
-            function () {
-                img = self.vue.image_list[img_idx];
-                img._pending = false;
-                self.vue.$set(self.vue.image_list, img_idx, img);
-            }
-        )
-    };
-
-    // Complete as needed.
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
-            image_list: [],
-            is_logged_in: is_logged_in,
-            star_indices: [1, 2, 3, 4, 5]
+            movie_list: [],
+            movie_search: '',
         },
         methods: {
-            mouse_over: self.mouse_over,
-            mouse_out: self.mouse_out,
-            set_stars: self.set_stars
+            get_movies: self.get_movies,
+            do_search: self.get_movies
         }
 
     });
 
-    self.get_info();
+    self.get_movies();
     $("#vue-div").show();
+
 
     return self;
 };
@@ -90,6 +49,4 @@ var APP = null;
 
 // This will make everything accessible from the js console;
 // for instance, self.x above would be accessible as APP.x
-jQuery(function () {
-    APP = app();
-});
+jQuery(function(){APP = app();});
