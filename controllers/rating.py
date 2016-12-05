517e056a3ae6859e0f8333aa7981622d18dfc2bd
Arthur Chiao <chiaoarthur@gmail.com>
2016-12-04 20:20:59 -0800

    Broken stars

---
+++ controllers/rating.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+# -------------------------------------------------------------------------
+# This is a sample controller
+# - index is the default action of any application
+# - user is required for authentication and authorization
+# - download is for downloading files uploaded in the db (does streaming)
+# -------------------------------------------------------------------------
+
+import time
+
+def index():
+    return dict()
+
+
+def get_info():
+    rows = db().select(db.movie_metadata.ALL, limitby=(0, 10))
+    def get_num_stars(img_idx):
+        if not auth.user_id:
+            return None
+        r = db((db.rating.user_id == auth.user_id) & (db.rating.movie_id == img_idx)).select().first()
+        return 0 if r is None else r.stars
+
+    movie_list = []
+    for i, movie in enumerate(rows):
+        n = get_num_stars(i)
+        print(movie)
+        movie_list.append(dict(
+            url=movie,
+            num_stars=n,
+            num_stars_display=n,  # To facilitate vue
+            id=i,
+        ))
+    return response.json(dict(movie_list=movie_list))
+
+
+@auth.requires_signature()
+def vote():
+    picid = int(request.vars.image_id)
+    num_stars = int(request.vars.num_stars)
+    db.rating.update_or_insert(
+        ((db.rating.image_id == picid) & (db.rating.user_id == auth.user_id)),
+        image_id=picid,
+        user_id=auth.user_id,
+        num_stars=num_stars
+    )
+    time.sleep(0.5)  # To make testing easier.
+    return "ok"
+
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    also notice there is http://..../[app]/appadmin/manage/auth to allow administrator to manage users
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
--- static/js/default_pref.js
+++ static/js/default_pref.js
@@ -1,6 +1,6 @@
 // This is the js for the default/index.html view.
 
-var app2 = function () {
+var app = function () {
 
     var self = {};
 
@@ -30,34 +30,34 @@ var app2 = function () {
 
     self.get_info = function () {
         $.getJSON(get_info_url, function (data) {
-            self.vue.image_list = data.image_list;
-            enumerate(self.vue.image_list);
-            decorate(self.vue.image_list);
+            self.vue.movie_list = data.movie_list;
+            enumerate(self.vue.movie_list);
+            decorate(self.vue.movie_list);
         });
     };
 
     self.mouse_over = function (img_idx, star_idx) {
-        self.vue.image_list[img_idx].num_stars_display = star_idx;
+        self.vue.movie_list[img_idx].num_stars_display = star_idx;
     };
 
     self.mouse_out = function (img_idx) {
-        self.vue.image_list[img_idx].num_stars_display = self.vue.image_list[img_idx].num_stars;
+        self.vue.movie_list[img_idx].num_stars_display = self.vue.movie_list[img_idx].num_stars;
     };
 
     self.set_stars = function (img_idx, star_idx) {
-        var img = self.vue.image_list[img_idx];
+        var img = self.vue.movie_list[img_idx];
         img.num_stars = star_idx;
         img._pending = true;
-        self.vue.$set(self.vue.image_list, img_idx, img);
+        self.vue.$set(self.vue.movie_list, img_idx, img);
         $.post(vote_url,
             {
                 image_id: img.id,
                 num_stars: star_idx
             },
             function () {
-                img = self.vue.image_list[img_idx];
+                img = self.vue.movie_list[img_idx];
                 img._pending = false;
-                self.vue.$set(self.vue.image_list, img_idx, img);
+                self.vue.$set(self.vue.movie_list, img_idx, img);
             }
         )
     };
@@ -68,7 +68,7 @@ var app2 = function () {
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
-            image_list: [],
+            movie_list: [],
             is_logged_in: is_logged_in,
             star_indices: [1, 2, 3, 4, 5]
         },
@@ -77,10 +77,10 @@ var app2 = function () {
             mouse_out: self.mouse_out,
             set_stars: self.set_stars
         }
+
     });
 
     self.get_info();
-
     $("#vue-div").show();
 
     return self;
@@ -91,5 +91,5 @@ var APP = null;
 // This will make everything accessible from the js console;
 // for instance, self.x above would be accessible as APP.x
 jQuery(function () {
-    APP = app2();
+    APP = app();
 });
